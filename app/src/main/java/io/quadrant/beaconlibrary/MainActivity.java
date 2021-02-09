db102c516adc83c313d1fdeabf2b53baca740190
roys <roys.yasbana@gmail.com>
2021-02-09 12:59:27 +0700

    implement github package

--- app/src/main/java/io/quadrant/beaconlibrary/MainActivity.java
+++ app/src/main/java/io/quadrant/beaconlibrary/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import io.quadrant.beaconlib.BluetoothUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
